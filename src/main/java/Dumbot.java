d0409ac53bcf36fdc7cd82516777255c8d6bf4dc
Panos Stylianides <panos@nexure.io>
2024-04-05 19:29:54 +0200

    testing

--- src/main/java/Dumbot.java
+++ src/main/java/Dumbot.java
@@ -8,57 +8,33 @@ import static robocode.util.Utils.normalRelativeAngleDegrees;
  * Dumbot - a robot by (Panos)
  */
 public class Dumbot extends Robot {
-  private boolean shouldMove = false;
+  int gunDirection = 1;
 
-  /**
-   * run: Dumbot's default behavior
-   */
   public void run() {
-    // Initialization of the robot should be put here
+    // Mere aesthetic changes
+    setBodyColor(Color.black);
+    setRadarColor(Color.green);
+    setGunColor(Color.black);
+    setBulletColor(Color.orange);
 
-    turnRight(normalRelativeAngleDegrees(0 - getHeading()));
-    ahead(5000);
-    turnLeft(90);
-    // After trying out your robot, try uncommenting the import at the top,
-    // and the next line:
-
-    // setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-
-    // Robot main loop
-//    while (true) {
-      // Replace the next 4 lines with any behavior you would like
-//      if (shouldMove) {
-//        ahead(30.0);
-//      }
-//    }
+    // Turns the gun infinitely, looking for enemies
+    while (true) {
+      turnGunRight(360);
+    }
   }
 
-  /**
-   * onScannedRobot: What to do when you see another robot
-   */
   public void onScannedRobot(ScannedRobotEvent e) {
-    this.fire(1.0);
-  }
-
-  /**
-   * onHitByBullet: What to do when you're hit by a bullet
-   */
-  public void onHitByBullet(HitByBulletEvent e) {
-    ahead(5000);
-    turnLeft(90);
-  }
-
-  public void onBulletMissed(BulletMissedEvent var1) {
-    this.scan();
-  }
-
-
-  /**
-   * onHitWall: What to do when you hit a wall
-   */
-  public void onHitWall(HitWallEvent e) {
-    // Replace the next line with any behavior you would like
-//    back(10);
-//    this.turnRight(90)
+    // Turn the robot towards the enemy
+    setTurnRight(e.getBearing());
+    // Shoots always that it's aiming at the enemy
+    setFire(3);
+    // And move forward
+    setAhead(100);
+    // Inverts the gun direction on each turn
+    gunDirection = -gunDirection;
+    // Turn 360 degrees (clockwise or anti clockwise,)
+    setTurnGunRight(360 * gunDirection);
+    // Execute all the pending actions
+    execute();
   }
 }
