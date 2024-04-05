a46efa62eeb795d24a28b8f534a4749d64e9be28
Panos Stylianides <panos@nexure.io>
2024-04-05 19:33:35 +0200

    fix

--- src/main/java/Dumbot.java
+++ src/main/java/Dumbot.java
@@ -1,41 +1,67 @@
 import robocode.*;
 //import java.awt.Color;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
-import robocode.*;
-import java.awt.Color;
+
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
 
 /**
  * Dumbot - a robot by (Panos)
  */
 public class Dumbot extends Robot {
-  int gunDirection = 1;
+  private boolean shouldMove = false;
 
+  /**
+   * run: Dumbot's default behavior
+   */
   public void run() {
-    // Mere aesthetic changes
-    setBodyColor(Color.black);
-    setRadarColor(Color.green);
-    setGunColor(Color.black);
-    setBulletColor(Color.orange);
+    // Initialization of the robot should be put here
 
-    // Turns the gun infinitely, looking for enemies
-    while (true) {
-      turnGunRight(360);
-    }
+    turnRight(normalRelativeAngleDegrees(0 - getHeading()));
+    ahead(5000);
+    turnLeft(90);
+    // After trying out your robot, try uncommenting the import at the top,
+    // and the next line:
+
+    // setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+    // Robot main loop
+//    while (true) {
+      // Replace the next 4 lines with any behavior you would like
+//      if (shouldMove) {
+//        ahead(30.0);
+//      }
+//    }
   }
 
+  /**
+   * onScannedRobot: What to do when you see another robot
+   */
   public void onScannedRobot(ScannedRobotEvent e) {
-    // Turn the robot towards the enemy
-    setTurnRight(e.getBearing());
-    // Shoots always that it's aiming at the enemy
-    setFire(3);
-    // And move forward
-    setAhead(100);
-    // Inverts the gun direction on each turn
-    gunDirection = -gunDirection;
-    // Turn 360 degrees (clockwise or anti clockwise,)
-    setTurnGunRight(360 * gunDirection);
-    // Execute all the pending actions
-    execute();
+//    if (e.getDistance() > 400) {
+//      return;
+//    }
+    this.fire(1.0);
+  }
+
+  /**
+   * onHitByBullet: What to do when you're hit by a bullet
+   */
+  public void onHitByBullet(HitByBulletEvent e) {
+    ahead(5000);
+    turnLeft(90);
+  }
+
+  public void onBulletMissed(BulletMissedEvent var1) {
+    this.scan();
+  }
+
+
+  /**
+   * onHitWall: What to do when you hit a wall
+   */
+  public void onHitWall(HitWallEvent e) {
+    // Replace the next line with any behavior you would like
+//    back(10);
+//    this.turnRight(90)
   }
 }
